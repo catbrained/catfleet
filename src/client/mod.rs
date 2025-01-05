04469ae8f9939b61d96043ae15dda0571f241210
Linda Siemons <linda@catbrained.dev>
2025-01-05 23:19:34 +0100

    feat(client): add extract resources with survey
    Signed-off-by: Linda Siemons <linda@catbrained.dev>

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1135,4 +1135,38 @@ impl Client {
             Ok(d) => Err(anyhow!("Unexpected response data: {d:?}")),
         }
     }
+
+    #[instrument(level = Level::DEBUG, skip(self))]
+    pub async fn extract_resources_with_survey(
+        &mut self,
+        ship: String,
+        survey: Survey,
+    ) -> Result<(Cooldown, Extraction, ShipCargo, Vec<ShipConditionEvent>), anyhow::Error> {
+        let body = match serde_json::to_vec(&survey) {
+            Ok(body) => body,
+            Err(e) => return Err(anyhow!(e)),
+        };
+
+        let req = Request::builder()
+            .uri(format!("/my/ships/{ship}/extract/survey"))
+            .method(Method::POST)
+            .body(Full::<Bytes>::new(body.into()))?;
+
+        let res = self.inner.ready().await?.call(req).await?;
+        event!(Level::DEBUG, "Response status: {}", res.status());
+
+        let body = res.collect().await?.aggregate();
+
+        let json = serde_json::from_reader(body.reader()).map(|res: ApiResponse| res.data);
+        match json {
+            Err(e) => Err(anyhow!(e)),
+            Ok(ApiResponseData::ExtractResources {
+                cooldown,
+                extraction,
+                cargo,
+                events,
+            }) => Ok((cooldown, extraction, cargo, events)),
+            Ok(d) => Err(anyhow!("Unexpected response data: {d:?}")),
+        }
+    }
 }
