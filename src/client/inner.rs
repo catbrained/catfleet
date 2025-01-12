da78792bf0441ca0453b5188be3eda0ca949c40c
Linda Siemons <linda@catbrained.dev>
2025-01-12 03:48:26 +0100

    feat(client): add ability to reconnect
    I moved the code for the connection establishment into a `Connector`
    struct, so it can be reused when the connection gets closed due to
    timeouts or other reasons.
    
    Still not sure about the logic in the `InnerClient::send_request()`
    function, but it works for now.
    Also not 100% sure if there is a better way for sharing
    a client/connection, but the wrapping in `Arc`s and cloning works
    for now.
    
    Signed-off-by: Linda Siemons <linda@catbrained.dev>

--- src/client/inner.rs
+++ src/client/inner.rs
@@ -16,6 +16,7 @@ use tracing::{event, instrument, span, Instrument, Level, Span};
 #[derive(Debug)]
 pub struct InnerClient<B> {
     sender: hyper::client::conn::http2::SendRequest<B>,
+    connector: Arc<Connector>,
 }
 
 impl<B> InnerClient<B>
@@ -26,70 +27,13 @@ where
 {
     #[instrument(level = Level::DEBUG)]
     pub async fn new(base_url: Uri) -> Result<Self, anyhow::Error> {
-        let host = base_url.host().context("URI has no host")?;
-        let port = base_url.port_u16().unwrap_or(443);
-        let address = format!("{}:{}", host, port);
+        let connector = Connector::new(base_url);
+        let sender = connector.connect().await?;
 
-        // Configure root certs.
-        let mut root_cert_store = RootCertStore::empty();
-        root_cert_store.extend(webpki_roots::TLS_SERVER_ROOTS.iter().cloned());
-
-        // Configure TLS client.
-        let mut config = ClientConfig::builder()
-            .with_root_certificates(root_cert_store)
-            .with_no_client_auth();
-        // We want to talk HTTP/2.
-        config.alpn_protocols = vec![b"h2".to_vec()];
-
-        // Set up network IO.
-        let connector = TlsConnector::from(Arc::new(config));
-        // 1. Establish TCP connection.
-        let tcp_stream = TcpStream::connect(address).await?;
-        // 2. Establish TLS connection.
-        let stream = connector
-            .connect(
-                ServerName::try_from(host.to_string()).expect("domain should be valid"),
-                tcp_stream,
-            )
-            .await?;
-        event!(
-            Level::TRACE,
-            "TLS connection established; performing http handshake"
-        );
-        // 3. Wrap stream in hyper/tokio compatibility layer.
-        let io = TokioIo::new(stream);
-
-        // 4. Perform HTTP handshake.
-        let (mut sender, conn) = hyper::client::conn::http2::Builder::new(TokioExecutor::new())
-            .handshake(io)
-            .instrument({
-                let span = span!(parent: None, Level::TRACE, "http_conn");
-                span.follows_from(Span::current());
-                span
-            })
-            .await?;
-        event!(
-            Level::TRACE,
-            "http2 handshake complete; spawning background dispatcher"
-        );
-
-        tokio::task::spawn(
-            async move {
-                if let Err(err) = conn.await {
-                    event!(Level::ERROR, "Client connection error: {:?}", err);
-                }
-            }
-            .instrument({
-                let span = span!(parent: None, Level::TRACE, "http_dispatcher");
-                span.follows_from(Span::current());
-                span
-            }),
-        );
-
-        // Wait for connection to become ready.
-        sender.ready().await?;
-
-        Ok(Self { sender })
+        Ok(Self {
+            sender,
+            connector: Arc::new(connector),
+        })
     }
 
     #[instrument(level = Level::DEBUG, skip(self, req), fields(req.url =% req.uri()))]
@@ -103,16 +47,29 @@ where
 
         // XXX: Is this really the correct way of doing this?
         loop {
-            self.sender.ready().await.unwrap();
+            match self.sender.ready().await {
+                Ok(_) => {}
+                Err(e) if e.is_closed() => {
+                    event!(Level::TRACE, "Connection closed. Reconnecting...");
+                    self.sender = self.connector.connect().await?;
+                    continue;
+                }
+                Err(e) => return Err(anyhow!(e)),
+            }
 
-            break match self.sender.send_request(req.clone()).await {
-                Ok(res) => Ok(res),
+            match self.sender.send_request(req.clone()).await {
+                Ok(res) => return Ok(res),
                 Err(e) if e.is_canceled() => {
                     event!(Level::WARN, "Request was cancelled. Retrying...");
                     continue;
                 }
-                Err(e) => Err(anyhow!(e)),
-            };
+                Err(e) if e.is_closed() => {
+                    event!(Level::TRACE, "Connection closed. Reconnecting...");
+                    self.sender = self.connector.connect().await?;
+                    continue;
+                }
+                Err(e) => return Err(anyhow!(e)),
+            }
         }
     }
 
@@ -126,6 +83,7 @@ impl<B> Clone for InnerClient<B> {
     fn clone(&self) -> Self {
         Self {
             sender: self.sender.clone(),
+            connector: self.connector.clone(),
         }
     }
 }
@@ -169,5 +127,101 @@ where
 
     fn call(&mut self, req: Request<B>) -> Self::Future {
         self.request(req)
+    }
+}
+
+struct Connector {
+    inner: TlsConnector,
+    url: Uri,
+}
+
+impl Connector {
+    #[instrument(level = Level::TRACE)]
+    fn new(url: Uri) -> Self {
+        // Configure root certs.
+        let mut root_cert_store = RootCertStore::empty();
+        root_cert_store.extend(webpki_roots::TLS_SERVER_ROOTS.iter().cloned());
+
+        // Configure TLS client.
+        let mut config = ClientConfig::builder()
+            .with_root_certificates(root_cert_store)
+            .with_no_client_auth();
+        // We want to talk HTTP/2.
+        config.alpn_protocols = vec![b"h2".to_vec()];
+
+        // Set up network IO.
+        let connector = TlsConnector::from(Arc::new(config));
+
+        Self {
+            inner: connector,
+            url,
+        }
+    }
+
+    #[instrument(level = Level::TRACE, skip(self))]
+    async fn connect<B>(&self) -> Result<hyper::client::conn::http2::SendRequest<B>, anyhow::Error>
+    where
+        B: Body + Send + Unpin + Clone + 'static,
+        B::Data: Send,
+        B::Error: Into<Box<dyn Error + Send + Sync>>,
+    {
+        let host = self.url.host().context("URI has no host")?;
+        let port = self.url.port_u16().unwrap_or(443);
+        let address = format!("{}:{}", host, port);
+
+        // 1. Establish TCP connection.
+        let tcp_stream = TcpStream::connect(address).await?;
+        // 2. Establish TLS connection.
+        let stream = self
+            .inner
+            .connect(
+                ServerName::try_from(host.to_string()).expect("domain should be valid"),
+                tcp_stream,
+            )
+            .await?;
+        event!(
+            Level::TRACE,
+            "TLS connection established; performing http handshake"
+        );
+        // 3. Wrap stream in hyper/tokio compatibility layer.
+        let io = TokioIo::new(stream);
+
+        // 4. Perform HTTP handshake.
+        let (mut sender, conn) = hyper::client::conn::http2::Builder::new(TokioExecutor::new())
+            .handshake(io)
+            .instrument({
+                let span = span!(parent: None, Level::TRACE, "http_conn");
+                span.follows_from(Span::current());
+                span
+            })
+            .await?;
+        event!(
+            Level::TRACE,
+            "http2 handshake complete; spawning background dispatcher"
+        );
+
+        tokio::task::spawn(
+            async move {
+                if let Err(err) = conn.await {
+                    event!(Level::ERROR, "Client connection error: {:?}", err);
+                }
+            }
+            .instrument({
+                let span = span!(parent: None, Level::TRACE, "http_dispatcher");
+                span.follows_from(Span::current());
+                span
+            }),
+        );
+
+        // Wait for connection to become ready.
+        sender.ready().await?;
+
+        Ok(sender)
+    }
+}
+
+impl std::fmt::Debug for Connector {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Connector {{ url: {}}}", self.url)
     }
 }
